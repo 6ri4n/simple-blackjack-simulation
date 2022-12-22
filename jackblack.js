6934f3f9683e415d9dcdde17f4cc772e933019d7
brian <bri4n.huang@gmail.com>
2022-12-21 19:21:52 -0800

    Update jackblack.js
    finished this project by:
    - adding a check for the win/lose condition
    - handling of each turn
    - displaying the ending hands

--- jackblack.js
+++ jackblack.js
@@ -41,6 +41,7 @@ const generateDeck = () => {
 };
 
 const getCard = (deck) => {
+    // returns a random card from the current deck
     const randomNumber = Math.floor(Math.random() * deck.size);
     let i = 0;
 
@@ -55,6 +56,7 @@ const getCard = (deck) => {
 };
 
 const getPointsAndDisplayCards = (currentCards) => {
+    // returns the total points and a string of the current hand
     let displayCurrentCards = '';
     let currentPoints = 0;
 
@@ -67,31 +69,69 @@ const getPointsAndDisplayCards = (currentCards) => {
 };
 
 const deck = generateDeck();
-
 let turn = 0;
 let player = new Set();
 let playerPoints = 0;
 let dealer = new Set();
 let dealerPoints = 0;
 
-while (!((playerPoints === 21 || playerPoints > 21) || (dealerPoints === 21 || dealerPoints > 21))) {
+// simulation
+while (true) {
+    // start of the game each person gets 2 cards
     if (turn === 0) {
         player.add(getCard(deck));
         player.add(getCard(deck));
         dealer.add(getCard(deck));
         dealer.add(getCard(deck));
 
-        const playerPointsAndCards = getPointsAndDisplayCards(player);
-        const dealerPointsAndCards = getPointsAndDisplayCards(dealer);
-        playerPoints += playerPointsAndCards[0];
-        dealerPoints += dealerPointsAndCards[0];
-        console.log('Starting Player Hand:\n' + playerPointsAndCards[1]);
-        console.log(`Starting Player Score: ${playerPoints}`);
-        console.log('Starting Dealer Hand:\n' + dealerPointsAndCards[1]);
-        console.log(`Starting Dealer Score: ${dealerPoints}`);
+        let playerPointsAndCards = getPointsAndDisplayCards(player);
+        let dealerPointsAndCards = getPointsAndDisplayCards(dealer);
+        playerPoints = playerPointsAndCards[0];
+        dealerPoints = dealerPointsAndCards[0];
+        console.log('Starting Player Hand:\n' + playerPointsAndCards[1] + `Starting Player Score: ${playerPoints}\n`);
+        console.log('Starting Dealer Hand:\n' + dealerPointsAndCards[1] + `Starting Dealer Score: ${dealerPoints}\n`);
+
+        // player goes first
+        player.add(getCard(deck));
+        playerPointsAndCards = getPointsAndDisplayCards(player);
+        playerPoints = playerPointsAndCards[0];
 
         turn++;
     }
-    break;
+
+    // check if game is over and display results
+    if (playerPoints > 21) {
+        console.log(`You lose! Your final score was: ${playerPoints} while the dealer had ${dealerPoints}\n`);
+        break;
+    }  else if (dealerPoints === 21) {
+        console.log(`You lose! Your final score was: ${playerPoints} while the dealer had ${dealerPoints}\n`);
+        break;
+    } else if (dealerPoints > 21) {
+        console.log(`You win! Your final score was: ${playerPoints} while the dealer had ${dealerPoints}\n`);
+        break;
+    } else if (playerPoints === 21) {
+        console.log(`You win! Your final score was: ${playerPoints} while the dealer had ${dealerPoints}\n`);
+        break;
+    }
+
+    // adding a card to each person and updating their points
+    if (turn % 2 != 0) {
+        // dealer turn
+        dealer.add(getCard(deck));
+        let dealerPointsAndCards = getPointsAndDisplayCards(dealer);
+        dealerPoints = dealerPointsAndCards[0];
+    } else {
+        // player turn
+        player.add(getCard(deck));
+        let playerPointsAndCards = getPointsAndDisplayCards(player);
+        playerPoints = playerPointsAndCards[0];
+    }
+
+    turn++;
 }
 
+// display ending hands
+const playerEndingHand = getPointsAndDisplayCards(player);
+const dealerEndingHand = getPointsAndDisplayCards(dealer);
+console.log(`Ending Player Hand:\n` + playerEndingHand[1]);
+console.log(`Ending Dealer Hand:\n` + dealerEndingHand[1]);
