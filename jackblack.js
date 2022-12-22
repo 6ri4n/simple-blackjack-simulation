87377102374814ca9fc4e9373363f0c2a98571ed
brian <bri4n.huang@gmail.com>
2022-12-21 18:32:38 -0800

    Update jackblack.js
    - getting points and displaying current cards
    - start of the game each person gets 2 cards

--- jackblack.js
+++ jackblack.js
@@ -1,4 +1,4 @@
-const generate_deck = () => {
+const generateDeck = () => {
     // creates a deck of 52 cards
     const deck = new Set();
     const faces = ['Jack', 'Queen', 'King', 'Ace'];
@@ -40,7 +40,7 @@ const generate_deck = () => {
     return deck
 };
 
-const get_card = (deck) => {
+const getCard = (deck) => {
     const randomNumber = Math.floor(Math.random() * deck.size);
     let i = 0;
 
@@ -54,7 +54,19 @@ const get_card = (deck) => {
     }
 };
 
-const deck = generate_deck();
+const getPointsAndDisplayCards = (currentCards) => {
+    let displayCurrentCards = '';
+    let currentPoints = 0;
+
+    for (const card of currentCards) {
+        currentPoints += card.value;
+        displayCurrentCards += `card: ${card.card}, suit: ${card.suit}\n`;
+    }
+
+    return [currentPoints, displayCurrentCards]
+};
+
+const deck = generateDeck();
 
 let turn = 0;
 let player = new Set();
@@ -64,7 +76,22 @@ let dealerPoints = 0;
 
 while (!((playerPoints === 21 || playerPoints > 21) || (dealerPoints === 21 || dealerPoints > 21))) {
     if (turn === 0) {
-        break;
+        player.add(getCard(deck));
+        player.add(getCard(deck));
+        dealer.add(getCard(deck));
+        dealer.add(getCard(deck));
+
+        const playerPointsAndCards = getPointsAndDisplayCards(player);
+        const dealerPointsAndCards = getPointsAndDisplayCards(dealer);
+        playerPoints += playerPointsAndCards[0];
+        dealerPoints += dealerPointsAndCards[0];
+        console.log('Starting Player Hand:\n' + playerPointsAndCards[1]);
+        console.log(`Starting Player Score: ${playerPoints}`);
+        console.log('Starting Dealer Hand:\n' + dealerPointsAndCards[1]);
+        console.log(`Starting Dealer Score: ${dealerPoints}`);
+
+        turn++;
     }
+    break;
 }
 
